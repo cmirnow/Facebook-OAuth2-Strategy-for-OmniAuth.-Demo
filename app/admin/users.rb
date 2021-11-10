5f8ed505767177674691141ef7f4c0cccf92433d
rishard <info@masterpro.ws>
2021-11-11 02:01:30 +0300

    activeadmin form fix

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,51 @@
 ActiveAdmin.register User do
+  permit_params :email,
+                :encrypted_password,
+                :reset_password_token,
+                :reset_password_sent_at,
+                :remember_created_at,
+                :provider,
+                :uid,
+                :name,
+                :image
+  remove_filter :encrypted_password,
+                :reset_password_token,
+                :reset_password_sent_at,
+                :remember_created_at,
+                :updated_at,
+                :image
+  actions :all, except: [:new]
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :provider, :uid, :name, :image
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :provider, :uid, :name, :image]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :created_at
+    column :provider
+    column :uid
+    column :name
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :provider
+      f.input :uid
+      f.input :name
+      f.input :image
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :created_at
+      row :provider
+      row :uid
+      row :name
+      row :image
+    end
+  end
 end
